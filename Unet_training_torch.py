92f33908ef17c2541348f3fca10d80971cbff74c
luke-avionics <sirsargras@gmail.com>
2020-09-15 02:41:23 -0500

    training verison working

--- Unet_training_torch.py
+++ Unet_training_torch.py
@@ -8,47 +8,104 @@ from glob import glob
 import math
 from sklearn.utils import shuffle
 import scipy.signal as signal
-
-
-
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import wandb
+from tqdm._utils import _term_move_up
+from tqdm import tqdm
+from datetime import datetime
+now = datetime.now()
+wandb.init(project='nih_project',name=str(now))
 freq = 10
 dim = 16
 learning_rate = 0.000001
-input_dim = 2
+input_dim = 10
 code = 64
-d2 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P2-2.npy")
-l2 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P2-2.npy")
-d3 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P3-2.npy")
-l3 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P3-2.npy")
-d4 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P4-2.npy")
-l4 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P4-2.npy")
-d5 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P5-2.npy")
-l5 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P5-2.npy")
-d7 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P7-2.npy")
-l7 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P7-2.npy")
-d8 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P8-2.npy")
-l8 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P8-2.npy")
-d9 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P9-2.npy")
-l9 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P9-2.npy")
-d13 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P13-2.npy")
-l13 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P13-2.npy")
-d17 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P17-2.npy")
-l17 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P17-2.npy")
-d18 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P18-2.npy")
-l18 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P18-2.npy")
-d19 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P19-2.npy")
-l19 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P19-2.npy")
-d24 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P24-2.npy")
-l24 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P24-2.npy")
-d25 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P25-2.npy")
-l25 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P25-2.npy")
-d26 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EGM_images_P26-2.npy")
-l26 = np.load("/content/drive/My Drive/Colab Notebooks/Data/EKG_images_P26-2.npy")
+data_normalize=False
+d2 = np.load("/data1/nih_data/Data/EGM_images_P2-2.npy")
+l2 = np.load("/data1/nih_data/Data/EKG_images_P2-2.npy")
+d3 = np.load("/data1/nih_data/Data/EGM_images_P3-2.npy")
+l3 = np.load("/data1/nih_data/Data/EKG_images_P3-2.npy")
+d4 = np.load("/data1/nih_data/Data/EGM_images_P4-2.npy")
+l4 = np.load("/data1/nih_data/Data/EKG_images_P4-2.npy")
+d5 = np.load("/data1/nih_data/Data/EGM_images_P5-2.npy")
+l5 = np.load("/data1/nih_data/Data/EKG_images_P5-2.npy")
+d7 = np.load("/data1/nih_data/Data/EGM_images_P7-2.npy")
+l7 = np.load("/data1/nih_data/Data/EKG_images_P7-2.npy")
+d8 = np.load("/data1/nih_data/Data/EGM_images_P8-2.npy")
+l8 = np.load("/data1/nih_data/Data/EKG_images_P8-2.npy")
+d9 = np.load("/data1/nih_data/Data/EGM_images_P9-2.npy")
+l9 = np.load("/data1/nih_data/Data/EKG_images_P9-2.npy")
+d13 = np.load("/data1/nih_data/Data/EGM_images_P13-2.npy")
+l13 = np.load("/data1/nih_data/Data/EKG_images_P13-2.npy")
+d17 = np.load("/data1/nih_data/Data/EGM_images_P17-2.npy")
+l17 = np.load("/data1/nih_data/Data/EKG_images_P17-2.npy")
+d18 = np.load("/data1/nih_data/Data/EGM_images_P18-2.npy")
+l18 = np.load("/data1/nih_data/Data/EKG_images_P18-2.npy")
+d19 = np.load("/data1/nih_data/Data/EGM_images_P19-2.npy")
+l19 = np.load("/data1/nih_data/Data/EKG_images_P19-2.npy")
+d24 = np.load("/data1/nih_data/Data/EGM_images_P24-2.npy")
+l24 = np.load("/data1/nih_data/Data/EKG_images_P24-2.npy")
+d25 = np.load("/data1/nih_data/Data/EGM_images_P25-2.npy")
+l25 = np.load("/data1/nih_data/Data/EKG_images_P25-2.npy")
+d26 = np.load("/data1/nih_data/Data/EGM_images_P26-2.npy")
+l26 = np.load("/data1/nih_data/Data/EKG_images_P26-2.npy")
 
 data = d5
 labels = l5
-data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.5, random_state=42)
+data=np.transpose(data,(0,3,1,2)).astype(np.float32)
+labels=np.transpose(labels,(0,3,1,2)).astype(np.float32)
 
+data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.5, random_state=42)
+print(type(data_train))
+print(data_train.shape)
+print(labels_train.shape)
+
+shape_train = np.shape(data_train)
+shape_test = np.shape(data_test)
+print("Number of training samples: ", shape_train[0])
+print("Number of testing samples: ", shape_test[0])
+print("Dimension of Data:", shape_train, shape_test)
+print("Number of all Data", shape_train[0]+shape_test[0])
+num_train_examples = shape_train[0]
+num_test_examples = shape_test[0]
+batch_size = 16
+batch_num = int(math.ceil(num_train_examples/batch_size))
+print("Number of batches: ", batch_num)
+n_epochs = 300
+save_steps = batch_num/1 * n_epochs  # Number of training batches between checkpoint saves
+
+
+mean_train_x=np.mean(data_train[:,:],axis=0)
+std_train_x=np.std(data_train[:,:],axis=0)
+if data_normalize:
+    data_train=(data_train-mean_train_x)/std_train_x
+
+mean_train_y=np.mean(labels_train,axis=0)
+std_train_y=np.std(labels_train,axis=0)
+if data_normalize:
+    labels_train=(labels_train-mean_train_y)/std_train_y
+    
+train_set=TensorDataset(torch.tensor(data_train),torch.tensor(labels_train))
+train_loader=DataLoader(train_set,batch_size=int(batch_size),num_workers=8)
+
+
+if data_normalize:
+    data_test=(data_test-mean_train_x)/std_train_x
+    #test_x[:,4].fill(1)
+    labels_test=(labels_test-mean_train_y)/std_train_y
+
+test_set=TensorDataset(torch.tensor(data_test),torch.tensor(labels_test))
+test_loader=DataLoader(test_set,batch_size=int(batch_size),num_workers=8)
+
+# data_test=torch.tensor(data_test)
+# labels_test=torch.tensor(labels_test)
+# data_test = Variable(data_test, volatile=True).cuda()
+# labels_test = Variable(labels_test, volatile=True).cuda()
 
 
 
@@ -56,13 +113,13 @@ def conv2d(in_channel, out_channel, kshape, stride, padding):
     tmp_layers=[]
     tmp_layers.append(nn.Conv2d(in_channel,out_channel, kshape, stride=stride, padding=padding))
     tmp_layers.append(nn.Tanh())
-    return nn.sequential(*tmp_layers)
+    return nn.Sequential(*tmp_layers)
     
 def deconv2d(in_channel, out_channel, kshape, stride, padding):
     tmp_layers=[]
     tmp_layers.append(nn.ConvTranspose2d(in_channel,out_channel, kshape, stride=stride, padding=padding))
     tmp_layers.append(nn.Tanh())
-    return nn.sequential(*tmp_layers)
+    return nn.Sequential(*tmp_layers)
     
 def maxpool2d(ksize=2, stride=2):
     return nn.MaxPool2d(ksize, stride=stride)
@@ -74,7 +131,7 @@ def fullyConnected(input_features,output_features):
     tmp_layers=[]
     tmp_layers.append(nn.Linear(input_features,output_features))
     tmp_layers.append(nn.Tanh())
-    return nn.sequential(*tmp_layers)
+    return nn.Sequential(*tmp_layers)
 
 def fullyConnected2(input_features,output_features):
     return nn.Linear(input_features,output_features, bias=False)
@@ -125,12 +182,12 @@ def find_correlation(output, original):
 
 def batch_norm(num_features):
     return nn.BatchNorm2d(num_features,eps=0.001,momentum=0.01)
-def batch_norm1d(num_features)
-    return nn.BatchNorm2d(num_features,eps=0.001,momentum=0.01)
+def batch_norm1d(num_features):
+    return nn.BatchNorm1d(num_features,eps=0.001,momentum=0.01)
 
-class ResNet(nn.Module):
+class egmnet(nn.Module):
     def __init__(self):
-        super(ResNet, self).__init__()
+        super(egmnet, self).__init__()
         keep_prob1=0.1
         keep_prob2=0.5
         keep_prob3=0
@@ -174,7 +231,7 @@ class ResNet(nn.Module):
         self.lfc7=fullyConnected2(dim*dim*24,dim*dim*24)
         self.loutput=dropout(keep_prob3)
         
-    def forward(x):
+    def forward(self,x):
         x=self.lc1(x)
         x=self.lb1(x)
         x=self.lp1(x)
@@ -183,7 +240,7 @@ class ResNet(nn.Module):
         x=self.lb2(x)
         x=self.lp2(x)
         x=self.ldo2(x)
-        x=x.view(-1,dim//4*dim//4*96,1,1)
+        x=x.view(-1,dim//4*dim//4*96)
         x=self.lfc1(x)
         x=self.lbfc1(x)
         x=self.ldo3(x)
@@ -210,16 +267,59 @@ class ResNet(nn.Module):
         x=self.lbdc2(x)
         x=self.lup2(x)
         x=self.ldo9(x)
-        x=x.view(-1,dim*dim*24,1,1)
+        x=x.view(-1,dim*dim*24)
         x=self.lfc7(x)
         x=self.loutput(x)
-        
+        return x
 
 
-
-# optimizer = torch.optim.Adam(net.parameters(), lr=initial_lr,weight_decay=weight_decay)
+net=egmnet()
+net = torch.nn.DataParallel(net).cuda()
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate,weight_decay=1e-3)
 # #optimizer = torch.optim.RMSprop(net.parameters(), lr=3e-3, momentum=0.9)
 # #scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,lr_step, eta_min=1e-7) 
 # scheduler = torch.optim.lr_scheduler.StepLR(optimizer,lr_step,lr_decay)
-# train_loss_logged=0
-        
+train_loss_logged=0
+pbar = tqdm(range(n_epochs))
+border = "="*50
+clear_border = _term_move_up() + "\r" + " "*len(border) + "\r"
+steps=0
+for epoch in pbar:  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        inputs = Variable(inputs, volatile=True).cuda()
+        labels = Variable(labels, volatile=True).cuda()
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # forward + backward + optimize
+        outputs = net(inputs)
+        #loss = criterion(outputs, labels)
+        loss=correlation(outputs, labels.view(-1,dim*dim*24))
+        #loss=error_loss(outputs, labels, 0,1)
+        loss.backward()
+        optimizer.step()
+        # print statistics
+        running_loss += loss.item()
+        if (i+1) % 5 == 0:
+            pbar.write('[%d, %5d] loss: %.3f ' %
+                  (epoch + 1, i + 1, running_loss / 5))
+            train_loss_logged=running_loss
+            running_loss = 0.0
+            wandb.log({'Train/loss': train_loss_logged/5,'steps': steps})
+    steps+=1
+    running_loss_test=0
+    for ti, tdata in enumerate(test_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        tinputs, tlabels = tdata
+
+        tinputs = Variable(tinputs, volatile=True).cuda()
+        tlabels = Variable(tlabels, volatile=True).cuda()
+        toutputs = net(tinputs)
+        tloss=correlation(toutputs, tlabels.view(-1,dim*dim*24))
+        running_loss_test+=tloss.item()
+    print('test loss: ',running_loss_test/(ti+1))
+    wandb.log({'Test/loss': running_loss_test/(ti+1), 'epochs': epoch})
